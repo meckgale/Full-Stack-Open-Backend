3118eb61785a4bc23acc85f28d6452b7558f11c6
meckgalen <meckgalen@gmail.com>
2025-01-25 02:09:38 +0300

    uibuild

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -21,29 +23,6 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 morgan.token("body", function (request, response) {
   return request.method === "POST" ? JSON.stringify(request.body) : "";
@@ -116,21 +95,20 @@ app.post("/api/persons", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.put("/api/persons/:id", (request, response) => {
-  const body = request.body;
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
 
-  if (body.name === undefined || body.number === undefined) {
+  if (name === undefined || number === undefined) {
     return response.status(400).json({
       error: "The name or number is missing",
     });
   }
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
--- models/person.js
+++ models/person.js
@@ -16,8 +16,21 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return /^\d{2,3}-\d{6,}$/.test(value);
+      },
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
