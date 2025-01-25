26145359cd0d4aea1bc652ca81c4ea2cd45f2ab9
meckgalen <meckgalen@gmail.com>
2025-01-25 04:02:27 +0300

    prettier and eslint config completed. All exercises in part3 are completed

--- index.js
+++ index.js
@@ -1,123 +1,123 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
+const express = require('express')
+const app = express()
+require('dotenv').config()
 
-const Person = require("./models/person");
+const Person = require('./models/person')
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-const morgan = require("morgan");
-const cors = require("cors");
+const morgan = require('morgan')
+const cors = require('cors')
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-morgan.token("body", function (request, response) {
-  return request.method === "POST" ? JSON.stringify(request.body) : "";
-});
+morgan.token('body', function (request, response) {
+  return request.method === 'POST' ? JSON.stringify(request.body) : ''
+})
 
-app.use(morgan(":method :url :res[content-length] - :response-time ms :body"));
+app.use(morgan(':method :url :res[content-length] - :response-time ms :body'))
 
-app.get("/api/persons", (request, response, next) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({})
     .then((people) => {
-      response.json(people);
+      response.json(people)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/info", (request, response, next) => {
+app.get('/info', (request, response, next) => {
   Person.countDocuments({})
     .then((count) => [
       response.send(
-        `<p>Phonebook has info for ${count} people <br/> ${new Date()}</p>`
+        `<p>Phonebook has info for ${count} people <br/> ${new Date()}</p>`,
       ),
     ])
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/api/persons/:id", (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
-      response.status(204).end();
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
   if (body.name === undefined || body.number === undefined) {
     return response.status(400).json({
-      error: "The name or number is missing",
-    });
+      error: 'The name or number is missing',
+    })
   }
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
-  console.log(`Inside backend, person object from request.body: ${person}`);
+  console.log(`Inside backend, person object from request.body: ${person}`)
 
   person
     .save()
     .then((savedPerson) => {
       console.log(
-        `Inside backend, after person.save(), savedPerson : ${savedPerson}`
-      );
-      response.json(savedPerson);
+        `Inside backend, after person.save(), savedPerson : ${savedPerson}`,
+      )
+      response.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const { name, number } = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
   if (name === undefined || number === undefined) {
     return response.status(400).json({
-      error: "The name or number is missing",
-    });
+      error: 'The name or number is missing',
+    })
   }
 
   Person.findByIdAndUpdate(
     request.params.id,
     { name, number },
-    { new: true, runValidators: true, context: "query" }
+    { new: true, runValidators: true, context: 'query' },
   )
     .then((updatedPerson) => {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
